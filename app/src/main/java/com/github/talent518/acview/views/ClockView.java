4d1ae05b671366f7eb1eefb72d2549825f831b7f
ZhangBaoCai <talent518@yeah.net>
2018-05-12 01:33:38 +0800

    放大模拟时钟绘图，五角星旋转动画。

--- app/src/main/java/com/github/talent518/acview/views/ClockView.java
+++ app/src/main/java/com/github/talent518/acview/views/ClockView.java
@@ -50,9 +50,13 @@ public class ClockView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         double angle;
-        final float SIDE = 10.0f;
-        float radius = Math.min(canvas.getWidth(), canvas.getHeight()) / 2.0f - 2 * SIDE;
-        float x, y;
+        final float RADIUS = Math.min(canvas.getWidth(), canvas.getHeight()) / 2.0f;
+        final float DIAL = RADIUS - 10.0f;
+        final float DIAL2 = DIAL - RADIUS * 0.05f;
+        final float TEXTSIZE = DIAL2 * 0.125f;
+        final float TEXT = DIAL2 - TEXTSIZE / 2.0f - 5.0f;
+        float radius = DIAL2 - TEXTSIZE - 10.0f;
+        float x, y, x2, y2;
         final float cx = canvas.getWidth() / 2.0f, cy = canvas.getHeight() / 2.0f;
 
         Calendar calendar = Calendar.getInstance();
@@ -67,24 +71,25 @@ public class ClockView extends View {
         paint.setAntiAlias(false);
         paint.setStyle(Paint.Style.STROKE);
 
+        Paint textPaint = new Paint();
+        textPaint.setColor(Color.BLACK);
+        textPaint.setStyle(Paint.Style.FILL);
+
+        textPaint.setTextAlign(Paint.Align.CENTER);
+
         canvas.drawColor(Color.WHITE);
 
-        String[] dfs = dateFormat.format(new Date()).split("\n");
-        paint.setTextSize(radius*0.1f);
-        paint.setTextAlign(Paint.Align.CENTER);
-        canvas.drawText(dfs[0], cx, cy - radius*0.6f, paint);
-        canvas.drawText(dfs[1], cx, cy - radius*0.6f + paint.getTextSize(), paint);
+        canvas.drawCircle(cx, cy, DIAL, paint);
+        canvas.drawCircle(cx, cy, DIAL2, paint);
 
-        canvas.drawCircle(cx, cy, radius, paint);
-        canvas.drawCircle(cx, cy, radius + SIDE, paint);
-
-        int sw = 0;
-        for (int i = 0; i < 360; i += 6) {
+        textPaint.setTextSize(TEXTSIZE);
+        int n = 1;
+        for (int i = 60; i > -300; i -= 6) {
             angle = i * Math.PI / 180.0;
-            x = cx + (float) (Math.cos(angle) * radius);
-            y = cy - (float) (Math.sin(angle) * radius);
-            float x2 = cx + (float) (Math.cos(angle) * (radius + SIDE));
-            float y2 = cy - (float) (Math.sin(angle) * (radius + SIDE));
+            x = cx + (float) (Math.cos(angle) * DIAL);
+            y = cy - (float) (Math.sin(angle) * DIAL);
+            x2 = cx + (float) (Math.cos(angle) * DIAL2);
+            y2 = cy - (float) (Math.sin(angle) * DIAL2);
 
             if (i % 90 == 0) {
                 paint.setStrokeWidth(10.0f);
@@ -94,30 +99,55 @@ public class ClockView extends View {
                 paint.setStrokeWidth(2.0f);
             }
             canvas.drawLine(x, y, x2, y2, paint);
+            if (i % 30 == 0) {
+                x = cx + (float) (Math.cos(angle) * TEXT);
+                y = cy - (float) (Math.sin(angle) * TEXT) + TEXTSIZE/3.0f;
+                canvas.drawText(Integer.toString(n++), x, y, textPaint);
+            }
         }
 
-        radius -= SIDE;
+        String[] dfs = dateFormat.format(new Date()).split("\n");
+        textPaint.setAlpha(64);
+        textPaint.setTextSize(radius*0.2f);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(dfs[0], cx, cy - radius*0.5f, textPaint);
+        textPaint.setTextSize(radius*0.2f);
+        canvas.drawText(dfs[1], cx, cy + radius*0.5f, textPaint);
+
+        paint.setStyle(Paint.Style.FILL);
 
         angle = (90 - 30 * hour - 30.0f / 60.0f * minute) * Math.PI / 180.0;
         x = cx + (float) (Math.cos(angle) * radius * 0.5f);
         y = cy - (float) (Math.sin(angle) * radius * 0.5f);
+        angle = (90 - 30 * hour - 30.0f / 60.0f * minute + 180) * Math.PI / 180.0;
+        x2 = cx + (float) (Math.cos(angle) * 24);
+        y2 = cy - (float) (Math.sin(angle) * 24);
         paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(6.0f);
-        canvas.drawLine(cx, cy, x, y, paint);
+        paint.setStrokeWidth(radius*0.035f);
+        canvas.drawLine(x2, y2, x, y, paint);
+        canvas.drawCircle(cx, cy, 12, paint);
 
         angle = (90 - 6 * minute - 6.0f / 60.0f * second) * Math.PI / 180.0;
         x = cx + (float) (Math.cos(angle) * radius * 0.75f);
         y = cy - (float) (Math.sin(angle) * radius * 0.75f);
+        angle = (90 - 6 * minute - 6.0f / 60.0f * second + 180) * Math.PI / 180.0;
+        x2 = cx + (float) (Math.cos(angle) * 24);
+        y2 = cy - (float) (Math.sin(angle) * 24);
         paint.setColor(Color.GREEN);
-        paint.setStrokeWidth(4.0f);
-        canvas.drawLine(cx, cy, x, y, paint);
+        paint.setStrokeWidth(radius*0.02f);
+        canvas.drawLine(x2, y2, x, y, paint);
+        canvas.drawCircle(cx, cy, 8, paint);
 
         angle = (90 - 6 * second) * Math.PI / 180.0;
         x = cx + (float) (Math.cos(angle) * radius);
         y = cy - (float) (Math.sin(angle) * radius);
+        angle = (90 - 6 * second + 180) * Math.PI / 180.0;
+        x2 = cx + (float) (Math.cos(angle) * 24);
+        y2 = cy - (float) (Math.sin(angle) * 24);
         paint.setColor(Color.RED);
-        paint.setStrokeWidth(2.0f);
-        canvas.drawLine(cx, cy, x, y, paint);
+        paint.setStrokeWidth(radius*0.01f);
+        canvas.drawLine(x2, y2, x, y, paint);
+        canvas.drawCircle(cx, cy, 4, paint);
     }
 
 }
--- app/src/main/java/com/github/talent518/acview/views/FiveAngleStarView.java
+++ app/src/main/java/com/github/talent518/acview/views/FiveAngleStarView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -11,20 +13,60 @@ import android.view.View;
 /**
  * 五角星
  */
-public class FiveAngleStarView extends View {
+public class FiveAngleStarView extends View implements View.OnClickListener {
     private static final String TAG = FiveAngleStarView.class.getSimpleName();
 
     public FiveAngleStarView(Context context) {
         super(context);
+
+        setClickable(true);
+        setOnClickListener(this);
+        mHandler.sendEmptyMessageDelayed(0, 20);
     }
 
     public FiveAngleStarView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        setClickable(true);
+        setOnClickListener(this);
+        mHandler.sendEmptyMessageDelayed(0, 20);
     }
 
     public FiveAngleStarView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+
+        setClickable(true);
+        setOnClickListener(this);
+        mHandler.sendEmptyMessageDelayed(0, 20);
     }
+
+    @Override
+    public void onClick(View v) {
+        isPlay = !isPlay;
+        mHandler.removeMessages(0);
+        mHandler.sendEmptyMessageDelayed(0, 20);
+    }
+
+    private boolean isPlay = true;
+
+    private int mAngle = 0;
+    private int mAngleStep = 1;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if(isPlay) {
+                sendEmptyMessageDelayed(0, 50);
+
+                mAngle += mAngleStep;
+                if((mAngleStep>0 && mAngle >= 360) || (mAngleStep<0 && mAngle <= 0) ) {
+                    mAngleStep = -mAngleStep;
+                }
+            } else {
+                mAngle = 0;
+            }
+            invalidate();
+        }
+    };
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -39,16 +81,21 @@ public class FiveAngleStarView extends View {
         paint.setColor(Color.BLACK);
         paint.setStrokeWidth(2.0f);
         paint.setAntiAlias(false);
+        paint.setStyle(Paint.Style.FILL);
 
         canvas.drawColor(Color.WHITE);
         for (int i = 0; i <= 720; i += 144) {
-            angle = (90 + i) * Math.PI / 180.0;
+            angle = (90 + i + mAngle) * Math.PI / 180.0;
             x = cx + (float) (Math.cos(angle) * radius);
             y = cy - (float) (Math.sin(angle) * radius);
-            angle = (90 + i + 144) * Math.PI / 180.0;
+            angle = (90 + i + 144 + mAngle) * Math.PI / 180.0;
             x2 = cx + (float) (Math.cos(angle) * radius);
             y2 = cy - (float) (Math.sin(angle) * radius);
             canvas.drawLine(x, y, x2, y2, paint);
+
+            if(i == 0) {
+                canvas.drawCircle(x, y, 10, paint);
+            }
         }
         paint.setStyle(Paint.Style.STROKE);
         canvas.drawCircle(cx, cy, radius, paint);
